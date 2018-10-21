3fa1f9305df278edeb6971f97bc229a51873fcbb
dracen <a.hayashi0113@gmail.com>
2018-10-21 22:55:41 +0900

    5

--- gr_sketch.cpp
+++ gr_sketch.cpp
@@ -30,12 +30,22 @@ static Camera camera(IMAGE_HW, IMAGE_VW);
 static DisplayApp  display_app;
 static CascadeClassifier detector_classifier;
 
-static cv::Point leftPupil;
-static cv::Point rightPupil;
-static cv::Rect leftEyeRegion;
-static cv::Rect rightEyeRegion;
+#define H_MAX 30
+#define H_MIN 0
+#define S_MAX 255
+#define S_MIN 50
+#define V_MAX 255
+#define V_MIN 50
 
-void findEyes(cv::Mat frame_gray, cv::Rect face);
+uint8_t bgr_buf [3 * IMAGE_HW * IMAGE_VW] __attribute((section("NC_BSS"),aligned(32)));
+uint8_t hsv_buf [3 * IMAGE_HW * IMAGE_VW] __attribute((section("NC_BSS"),aligned(32)));
+uint8_t gray_buf [1 * IMAGE_HW * IMAGE_VW] __attribute((section("NC_BSS"),aligned(32)));
+
+Scalar red(0, 0, 255), green(0, 255, 0), blue(255, 0, 0);
+Scalar yellow = red + green;
+Scalar sky = green + blue;
+Scalar white = Scalar::all(255);
+Scalar pink = Scalar(154, 51, 255);
 
 void setup() {
     pinMode(PIN_LED_GREEN, OUTPUT);
@@ -64,8 +74,8 @@ static uint8_t flag = 0;
 void loop(){
     Mat img_raw(IMAGE_VW, IMAGE_HW, CV_8UC2, camera.getImageAdr());
 
-    Mat src;
-    cvtColor(img_raw, src, COLOR_YUV2GRAY_YUYV); //covert from YUV to GRAY
+    Mat img_gray(IMAGE_VW, IMAGE_HW, CV_8UC1, gray_buf);
+    cvtColor(img_raw, img_gray, COLOR_YUV2GRAY_YUYV); //covert from YUV to GRAY
 
     // Detect a face in the frame
     Rect face_roi;
@@ -75,7 +85,7 @@ void loop(){
 
     // Perform detected the biggest face
     std::vector<Rect> rect_faces;
-    detector_classifier.detectMultiScale(src, rect_faces,
+    detector_classifier.detectMultiScale(img_gray, rect_faces,
                                          DETECTOR_SCALE_FACTOR,
                                          DETECTOR_MIN_NEIGHBOR,
 										 CASCADE_SCALE_IMAGE | CASCADE_FIND_BIGGEST_OBJECT,
@@ -84,9 +94,6 @@ void loop(){
     if (rect_faces.size() > 0) {
         // A face is detected
         face_roi = rect_faces[0];
-        if(flag==1){
-        	findEyes(src, rect_faces[0]);
-        }
     } else {
         // No face is detected, set an invalid rectangle
         face_roi.x = -1;
@@ -101,90 +108,77 @@ void loop(){
         digitalWrite(PIN_LED_GREEN, LOW);
     } else {
     }
+    Mat img_bgr(IMAGE_VW, IMAGE_HW, CV_8UC3, bgr_buf);
+    Mat img_hsv(IMAGE_VW, IMAGE_HW, CV_8UC3, hsv_buf);
 
-    Mat dst;
-    cvtColor(img_raw, dst, COLOR_YUV2BGR_YUYV); //covert from YUV to BGR
-    Scalar red(0, 0, 255), green(0, 255, 0), blue(255, 0, 0);
+    cvtColor(img_raw, img_bgr, COLOR_YUV2BGR_YUYV); //covert from YUV to BGR
+    cvtColor(img_bgr, img_hsv, COLOR_BGR2HSV); //covert from YUV to BGR
 
+    medianBlur(img_hsv, img_hsv, 3);
+
+    Scalar s_min = Scalar(H_MIN, S_MIN, V_MIN);
+	Scalar s_max = Scalar(H_MAX, S_MAX, V_MAX);
+	inRange(img_hsv, s_min, s_max, img_gray);
+
+	vector<vector<Point> > contours;
+	vector<Vec4i> hierarchy;
+	size_t indexOfBiggestContour;
+	findContours(img_gray, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_NONE);
+	vector<Point2f> mc(contours.size()); //get centers
+
+	if(contours.size() > 0){
+		vector<Moments> mu(contours.size());
+		for (size_t i = 0; i < contours.size(); i++){
+			mu[i] = moments(contours[i], false);
+		}
+		for (size_t i = 0; i < contours.size(); i++)
+		{
+			mc[i] = Point2f(mu[i].m10 / mu[i].m00, mu[i].m01 / mu[i].m00);
+		}
+		indexOfBiggestContour = -1;
+		size_t sizeOfBiggestContour = 0;
+		for (size_t i = 0; i < contours.size(); i++) {
+			if (contours[i].size() > sizeOfBiggestContour) {
+				sizeOfBiggestContour = contours[i].size();
+				indexOfBiggestContour = i;
+			}
+		}
+	}
+
+	Rect rect = boundingRect (contours[indexOfBiggestContour]);
+//	rect.x -= 40;
+//	rect.y -= 40;
+//	rect.width += 80;
+//	rect.height += 80;
 
     if(digitalRead(PIN_SW0)==0){
     	flag = 0x01;
+    }else if(digitalRead(PIN_SW1)==0){
+    	flag = 0x02;
     }else{
     	flag = 0x00;
     }
 
+    Mat img_bgr2(IMAGE_VW, IMAGE_HW, CV_8UC3, hsv_buf);
+    img_bgr2 = Mat::zeros(IMAGE_VW, IMAGE_HW, CV_8UC3);
+
     if(flag==0x00){
-    	rectangle(dst, Point(face_roi.x, face_roi.y), Point(face_roi.x + face_roi.width, face_roi.y + face_roi.height), red, 2);
-    	size_t jpegSize = camera.createJpeg(IMAGE_HW, IMAGE_VW, dst.data, Camera::FORMAT_RGB888);
+    	rectangle(img_bgr, rect, blue, 2);
+    	rectangle(img_bgr, Point(face_roi.x, face_roi.y), Point(face_roi.x + face_roi.width, face_roi.y + face_roi.height), red, 2);
+		drawContours(img_bgr, contours, indexOfBiggestContour, sky, 2, 8, vector<Vec4i>(), 0, Point());
+		circle(img_bgr, mc[indexOfBiggestContour], 5, red, -1, 8, 0);
+    	size_t jpegSize = camera.createJpeg(IMAGE_HW, IMAGE_VW, img_bgr.data, Camera::FORMAT_RGB888);
 		display_app.SendJpeg(camera.getJpegAdr(), jpegSize);
     }else if(flag==0x01){
-		if (rect_faces.size() > 0) {
-			cv::Mat faceROI = dst(face_roi);
-//			circle(faceROI, rightPupil, 3, 1234);
-//			circle(faceROI, leftPupil, 3, 1234);
-	    	rectangle(dst, Point(face_roi.x, face_roi.y), Point(face_roi.x + face_roi.width, face_roi.y + face_roi.height), red, 2);
-			rectangle(faceROI, leftEyeRegion, red, 2);
-			rectangle(faceROI, rightEyeRegion, red, 2);
-
-			size_t jpegSize = camera.createJpeg(IMAGE_HW, IMAGE_VW, dst.data, Camera::FORMAT_RGB888);
-			display_app.SendJpeg(camera.getJpegAdr(), jpegSize);
-		}
+    	img_bgr.copyTo(img_bgr2, img_gray);
+    	rectangle(img_bgr2, Point(face_roi.x, face_roi.y), Point(face_roi.x + face_roi.width, face_roi.y + face_roi.height), red, 2);
+    	size_t jpegSize = camera.createJpeg(IMAGE_HW, IMAGE_VW, img_bgr2.data, Camera::FORMAT_RGB888);
+		display_app.SendJpeg(camera.getJpegAdr(), jpegSize);
+    }else if(flag==0x02){
+    	rectangle(img_gray, Point(face_roi.x, face_roi.y), Point(face_roi.x + face_roi.width, face_roi.y + face_roi.height), red, 2);
+    	size_t jpegSize = camera.createJpeg(IMAGE_HW, IMAGE_VW, img_gray.data, Camera::FORMAT_GRAY);
+		display_app.SendJpeg(camera.getJpegAdr(), jpegSize);
     }
-}
-
-
-void findEyes(cv::Mat frame_gray, cv::Rect face) {
-  cv::Mat faceROI = frame_gray(face);
-  cv::Mat debugFace = faceROI;
-
-  if (kSmoothFaceImage) {
-    double sigma = kSmoothFaceFactor * face.width;
-    GaussianBlur( faceROI, faceROI, cv::Size( 0, 0 ), sigma);
-  }
-  //-- Find eye regions and draw them
-  int eye_region_width = face.width * (kEyePercentWidth/100.0);
-  int eye_region_height = face.width * (kEyePercentHeight/100.0);
-  int eye_region_top = face.height * (kEyePercentTop/100.0);
-
-  leftEyeRegion.x = face.width*(kEyePercentSide/100.0);
-  leftEyeRegion.y = eye_region_top;
-  leftEyeRegion.width = eye_region_width;
-  leftEyeRegion.height = eye_region_height;
-
-  rightEyeRegion.x = face.width - eye_region_width - face.width*(kEyePercentSide/100.0);
-  rightEyeRegion.y = eye_region_top;
-  rightEyeRegion.width = eye_region_width;
-  rightEyeRegion.height = eye_region_height;
-
-  //-- Find Eye Centers
-//  leftPupil = findEyeCenter(faceROI,leftEyeRegion,"Left Eye");
-//  rightPupil = findEyeCenter(faceROI,rightEyeRegion,"Right Eye");
-
-  // get corner regions
-  cv::Rect leftRightCornerRegion(leftEyeRegion);
-  leftRightCornerRegion.width -= leftPupil.x;
-  leftRightCornerRegion.x += leftPupil.x;
-  leftRightCornerRegion.height /= 2;
-  leftRightCornerRegion.y += leftRightCornerRegion.height / 2;
-  cv::Rect leftLeftCornerRegion(leftEyeRegion);
-  leftLeftCornerRegion.width = leftPupil.x;
-  leftLeftCornerRegion.height /= 2;
-  leftLeftCornerRegion.y += leftLeftCornerRegion.height / 2;
-  cv::Rect rightLeftCornerRegion(rightEyeRegion);
-  rightLeftCornerRegion.width = rightPupil.x;
-  rightLeftCornerRegion.height /= 2;
-  rightLeftCornerRegion.y += rightLeftCornerRegion.height / 2;
-  cv::Rect rightRightCornerRegion(rightEyeRegion);
-  rightRightCornerRegion.width -= rightPupil.x;
-  rightRightCornerRegion.x += rightPupil.x;
-  rightRightCornerRegion.height /= 2;
-  rightRightCornerRegion.y += rightRightCornerRegion.height / 2;
-
-  // change eye centers to face coordinates
-  rightPupil.x += rightEyeRegion.x;
-  rightPupil.y += rightEyeRegion.y;
-  leftPupil.x += leftEyeRegion.x;
-  leftPupil.y += leftEyeRegion.y;
 }
 
 
